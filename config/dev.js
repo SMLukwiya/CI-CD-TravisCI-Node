68caff1f5a8311c4867e006914e15a4af8c6505e
SMLukwiya <sundaymorganl@gmail.com>
2022-01-15 10:24:59 +0300

    remove keys

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  googleClientID: process.env.googleClientID,
+  googleClientSecret: process.env.googleClientSecret,
   mongoURI: process.env.DEV_MONGOURI,
   cookieKey: '123123123',
 };
